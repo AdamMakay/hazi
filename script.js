c17bc5a4d8d8d5a3263dc70d5f03e37d885c1b96
AdamMakay <makayadam1@gmail.com>
2026-02-18 21:54:07 +0100

    fix submodule issue

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function TablaLetrehozasa()
 {
     let container = document.getElementById("container");
     let table = document.createElement("table");
-    let sorok = Number(document.getElementById("rows").value);
-    let oszlopok = Number(document.getElementById("columns").value);
+    let sorok = Number(document.getElementById("columns").value);
+    let oszlopok = Number(document.getElementById("rows").value);
     let oszlop;
     for(let i = 0;i<oszlopok;i++)
     {
@@ -16,8 +16,38 @@ function TablaLetrehozasa()
             sor = document.createElement("td")
             //sor.value(i+1)
             oszlop.append(sor);
-            table.append(oszlop);
         }
+        table.append(oszlop);
     }
+    let srotores = document.createElement("hr")
+    
     container.append(table);
+    container.append(srotores);
+}
+
+function Keret()
+{
+    
+    let border = Number(document.getElementById("border_width").value);
+    let osszesTabla = document.querySelectorAll("#container > table")
+    let utolsoTabla = osszesTabla[osszesTabla.length-1]
+    let tabla = utolsoTabla.querySelectorAll("td")
+    for(let i = 0; i<tabla.length;i++)
+    {
+        tabla[i].style.border = border + "px solid black";
+    }
+}
+
+
+function collapse()
+{
+    let osszesTabla = document.querySelectorAll("#container > table")
+    let utolsoTabla = osszesTabla[osszesTabla.length-1]
+    let tabla = utolsoTabla.querySelectorAll("td")
+    let checkboz = document.getElementById("collapse");
+    if(checkboz.checked == true)
+    {
+        utolsoTabla.style.borderCollapse = "collapse";
+    }
+    
 }
